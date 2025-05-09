2b07736dc91e69f73d8c519e3ca42fe00a57d37d
HP <HP@NGHA>
2025-05-09 11:31:13 +0700

    fix code

--- lib/constants.dart
+++ lib/constants.dart
@@ -58,7 +58,86 @@ const Color darkGreyColor = Color(0xFF1C1C25);
 // const Color greyColor5 = Color(0xFFFBFBFC);
 
 const Color purpleColor = Color(0xFF7B61FF);
+const Color successColor = Color(0xFF2ED573);import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+// Demo product image URLs
+const String productDemoImg1 = "https://i.imgur.com/CGCyp1d.png";
+const String productDemoImg2 = "https://i.imgur.com/AkzWQuJ.png";
+const String productDemoImg3 = "https://i.imgur.com/J7mGZ12.png";
+const String productDemoImg4 = "https://i.imgur.com/q9oF9Yq.png";
+const String productDemoImg5 = "https://i.imgur.com/MsppAcx.png";
+const String productDemoImg6 = "https://i.imgur.com/JfyZlnO.png";
+
+// Typography
+const String grandisExtendedFont = "Grandis Extended";
+
+// Color palette
+const Color primaryColor = Color(0xFF7B61FF);
+
+const MaterialColor primaryMaterialColor = MaterialColor(0xFF9581FF, <int, Color>{
+  50: Color(0xFFEFECFF),
+  100: Color(0xFFD7D0FF),
+  200: Color(0xFFBDB0FF),
+  300: Color(0xFFA390FF),
+  400: Color(0xFF8F79FF),
+  500: Color(0xFF7B61FF),
+  600: Color(0xFF7359FF),
+  700: Color(0xFF684FFF),
+  800: Color(0xFF5E45FF),
+  900: Color(0xFF6C56DD),
+});
+
+// Black shades
+const Color blackColor = Color(0xFF16161E);
+const Color blackColor80 = Color(0xFF45454B);
+const Color blackColor60 = Color(0xFF737378);
+const Color blackColor40 = Color(0xFFA2A2A5);
+const Color blackColor20 = Color(0xFFD0D0D2);
+const Color blackColor10 = Color(0xFFE8E8E9);
+const Color blackColor5 = Color(0xFFF3F3F4);
+
+// White shades (fixed typo: 'while' to 'white')
+const Color whiteColor = Colors.white;
+const Color whiteColor80 = Color(0xFFCCCCCC);
+const Color whiteColor60 = Color(0xFF999999);
+const Color whiteColor40 = Color(0xFF666666);
+const Color whiteColor20 = Color(0xFF333333);
+const Color whiteColor10 = Color(0xFF191919);
+const Color whiteColor5 = Color(0xFF0D0D0D);
+
+// Grey shades
+const Color greyColor = Color(0xFFB8B5C3);
+const Color lightGreyColor = Color(0xFFF8F8F9);
+const Color darkGreyColor = Color(0xFF1C1C25);
+
+// Status colors
+const Color purpleColor = Color(0xFF7B61FF);
 const Color successColor = Color(0xFF2ED573);
+const Color warningColor = Color(0xFFFFBE21);
+const Color errorColor = Color(0xFFEA5B5B);
+
+// UI constants
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 12.0; // Fixed typo: 'Radious' to 'Radius'
+const Duration defaultDuration = Duration(milliseconds: 300);
+
+// Form validators
+final passwordValidator = MultiValidator([
+  RequiredValidator(errorText: 'Password is required'),
+  MinLengthValidator(8, errorText: 'Password must be at least 8 characters long'),
+  PatternValidator(
+    r'(?=.*?[#?!@$%^&*-])',
+    errorText: 'Password must contain at least one special character',
+  ),
+]);
+
+final emailValidator = MultiValidator([ // Fixed typo: 'emaild' to 'email'
+  RequiredValidator(errorText: 'Email is required'),
+  EmailValidator(errorText: 'Enter a valid email address'),
+]);
+
+const String passwordNotMatchErrorText = "Passwords do not match";
 const Color warningColor = Color(0xFFFFBE21);
 const Color errorColor = Color(0xFFEA5B5B);
 
